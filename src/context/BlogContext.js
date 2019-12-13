554821ba4b196a2568f96c89a48d8541dabbc4c2
matthias <matthias.dfrc.ch>
2019-12-13 17:28:36 +0900

    2019.12.13_01 1. move the input from to blogpostfrom component 2. added backward function 3. edit page and icon

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -9,17 +9,25 @@ const blogReducer = (state, action) => {
                 ...state, 
             { 
                 id: Math.floor(Math.random()* 99999 ) ,
-                title: `Blog Post #${state.length + 1}`
+                title: action.payload.title,
+                content: action.payload.content,
             }
         ];
+        case 'edit_blogpost':
+            return state.map((blogPost) => {
+                return blogPost.id === action.payload.id ? action.payload : blogPost 
+            })
         default:
             return state;
     }
 };
 
 const addBlogPost = dispatch => {
-    return () => {
-        dispatch({ type: 'add_blogpost'});
+    return (title, content, callback) => {
+        dispatch({ type: 'add_blogpost', payload: {title, content}});
+        if ( callback ) {
+            callback();
+        }
     };
 };
 
@@ -29,8 +37,20 @@ const deleteBlogPost = dispatch => {
     }
 }
 
+const editBlogPost = dispatch => {
+    return (id, title, content, callback) => {
+        dispatch({ 
+            type: 'edit_blogpost', 
+            payload: { id, title, content }
+        });
+        if ( callback ) {
+            callback();
+        }
+    };
+};
+
 export const { Context, Provider } = createDataContext(
     blogReducer, 
-    {addBlogPost, deleteBlogPost},
+    {addBlogPost, deleteBlogPost, editBlogPost},
     [] 
 );
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,17 +1,15 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'react-native';
 import { Context } from '../context/BlogContext';
 import { Feather } from '@expo/vector-icons'
-import { TouchableOpacity } from 'react-native-gesture-handler';
 
 
 
 const IndexScreen = ({navigation}) => {
-    const {  state, addBlogPost, deleteBlogPost} = useContext(Context);
+    const {  state , deleteBlogPost} = useContext(Context);
 
     return (
             <View>
-                <Button title="Add Post" onPress={addBlogPost} />
                 <FlatList 
                     data={state} 
                     keyExtractor={(blogPosts) => blogPosts.title}
@@ -35,6 +33,14 @@ const IndexScreen = ({navigation}) => {
             </View>
     )
 }
+
+IndexScreen.navigationOptions = ({ navigation }) => {
+    return {
+        headerRight:<TouchableOpacity onPress={() => navigation.navigate('Create')}>
+            <Feather name="plus" size={30} />
+        </TouchableOpacity>
+    };
+};
 
 const styles = StyleSheet.create({
     row: {
